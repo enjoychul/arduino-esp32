b61b975ac292f445862f486fd78802a4efbb2411
enjoychul <>
2024-07-20 08:20:40 +0000

    Updates

---
+++ _next/static/toP6gSyl9IGLWjKb5SzoH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/776863efaada285f.css","static/chunks/pages/index-59b19f1e908712bd.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
